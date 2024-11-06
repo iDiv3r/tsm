a9e7c9826d909b0d6f9a96e6baf88cf1b1ac6bcb
Div3r <udhgjkf@gmail.com>
2024-11-06 14:46:19 -0600

    Cambios

--- resources/views/vistas/vuelosADM.blade.php
+++ resources/views/vistas/vuelosADM.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="flex grid-cols-1 gap-20 w-100 md:row">
     
+    
 
     {{-- container tabla --}}
     <div class="container pt-10 ps-10 ">
@@ -36,6 +37,7 @@
                         </th>
                     </tr>
                 </thead>
+
                 <tbody>
                     <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                         <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
