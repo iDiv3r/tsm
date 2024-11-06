92bd474a79729107260461da86ffbc91b75771ef
Roberto Resendiz <robertoresendiz504@gmail.com>
2024-11-06 03:11:03 -0600

    Vistas Admin Reporte Finalizado

--- routes/robert.php
+++ routes/robert.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DestinoAdm;
+use App\Http\Controllers\ReporteAdm;
 
 Route::view('/prueba','prueba');
 
@@ -14,3 +15,12 @@ route::get('/destinosAdm',[DestinoAdm::class,'vistaDestinosAdm'])->name('rutaadm
 route::Post('/adminDestinosadd',[DestinoAdm::class,'addDestino'])->name('rutaaddDestino');
 route::Post('/adminDestinosedit',[DestinoAdm::class,'editDestino'])->name('rutaeditDestino');
 route::Post('/adminDestinosdele',[DestinoAdm::class,'delDestino'])->name('rutadeleDestino');
+
+route::get('/reportesAdm',[ReporteAdm::class,'vistaReporteAdm'])->name('rutaadminReporte');
+route::Post('/adminrepodestadd',[ReporteAdm::class,'addDestinoRepo'])->name('rutaadminrepodestadd');
+route::Post('/adminrepoaeroadd',[ReporteAdm::class,'addAeroRepo'])->name('rutaadminrepoaeroadd');
+route::Post('/adminrepoaclieadd',[ReporteAdm::class,'addClienRepo'])->name('rutaadminrepoaclieadd');
+
+route::Post('/adminrepodestdel',[ReporteAdm::class,'delDestinoRepo'])->name('rutaadminrepodestdel');
+route::Post('/adminrepoaerodel',[ReporteAdm::class,'delAeroRepo'])->name('rutaadminrepoaerodel');
+route::Post('/adminrepoacliedel',[ReporteAdm::class,'delClienRepo'])->name('rutaadminrepoacliedel');
