3cac8ef97f4a16ee0ded8765e313e318ef0e9e2d
Div3r <udhgjkf@gmail.com>
2024-11-22 17:53:42 -0600

    City seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Database\Seeders\CountrySeeder;
+use Database\Seeders\CitySeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -28,6 +29,7 @@ class DatabaseSeeder extends Seeder
         
         $this->call([
             CountrySeeder::class,
+            CitySeeder::class
         ]);
 
     }
